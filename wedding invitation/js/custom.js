bed71c293622f64170fc060eb6470a3eef92039f
Chouettics <45276804+Maitan21@users.noreply.github.com>
2024-06-01 12:51:19 +0900

    Delete wedding invitation directory

--- wedding invitation/js/custom.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('phonenumber').addEventListener('input', function() {
-    var invalidMsg = document.querySelector('.invalid-msg');
-    if (this.validity.valid) {
-        invalidMsg.style.display = 'none';
-    } else {
-        invalidMsg.style.display = 'block';
-    }
-});
